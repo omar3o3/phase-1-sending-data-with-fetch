d22979606a91c968f3fe7218a15633681d2b91d7
Omar <otantawy53@gmail.com>
2022-05-05 14:56:30 -0400

    commit tree 7369a5f3e6073f84f299c874103288e44d725e95

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Add your code here
+
+// ****************************************************************************************
+
+// const submitData = {
+//     fetch('http://localhost:3000/users'), {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Accept': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             'name': 'name',
+//             'email': 'email'
+//         })
+//     }
+// }
+
+// ****************************************************************************************
+
+// const configureData = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json',
+//     },
+//     body: JSON.stringify({
+//         'name': name,
+//         'email': email,
+//     })
+// }
+
+// let submitData = () => fetch()
+function submitData (name , email){
+    const configureData = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email,
+        })
+    }
+    return fetch('http://localhost:3000/users' , configureData).then(resp => resp.json()).then(data => {
+        document.body.textContent = data.id;
+    }).catch(function (error){
+        document.body.textContent = error.message;
+    })
+}
